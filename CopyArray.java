c8db90b2bc5da8862934680970f6760690c93ecb
Saurabh Dhandare <saurabhdhandare@gmail.com>
2024-08-28 20:28:57 +0530

    Add files via upload

---
+++ CopyArray.java
@@ -0,0 +1,17 @@
+public class CopyArray {
+    public static void main(String[] args) {
+        int[] arr1 = new int[] {1, 2, 3, 4, 5};
+        int[] arr2 = new int[arr1.length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            arr2[i] = arr1[i];
+        }
+        
+        System.out.println("Elements of arr2 array: ");
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+
+        System.out.println();
+    }
+}
