eb605edbc58b4fcb7edc808543afccaff13c9b1f
Saurabh Dhandare <saurabhdhandare@gmail.com>
2024-08-29 12:27:20 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int count = 25;
+        int a = 0, b = 1;
+        int c;
+
+        System.out.print(a + " " + b);
+
+        for (int i = 2; i < count; ++i) {
+            c = a + b;
+            System.out.print(" " + c);
+            a = b;
+            b = c;
+        }
+
+        System.out.println();
+    }
+}
